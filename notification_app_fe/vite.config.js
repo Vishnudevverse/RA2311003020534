a43258ce220f4056eccace878d6ef43806ae8e97
Vishnuvarthan <selvanvishnu73@gmail.com>
2026-05-02 12:58:44 +0530

    deployed

--- notification_app_fe/vite.config.js
+++ notification_app_fe/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/RA2311003020534/", // Add this exact line
 })
